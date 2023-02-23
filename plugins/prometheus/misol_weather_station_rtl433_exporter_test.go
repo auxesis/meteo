2916ce5aea7a00e662f50d638fdb2eb2f8083447
Lindsay Holmwood <lindsay@holmwood.id.au>
2023-02-23 22:01:33 +1100

    Turn tests into tables

--- plugins/prometheus/misol_weather_station_rtl433_exporter_test.go
+++ plugins/prometheus/misol_weather_station_rtl433_exporter_test.go
@@ -13,7 +13,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type TestMsg struct{}
+type TestMsg struct {
+	TopicString  string
+	PayloadBytes []byte
+}
 
 func (tm TestMsg) Duplicate() bool {
 	return false
@@ -25,13 +28,13 @@ func (tm TestMsg) Retained() bool {
 	return false
 }
 func (tm TestMsg) Topic() string {
-	return ""
+	return tm.TopicString
 }
 func (tm TestMsg) MessageID() uint16 {
 	return uint16(0)
 }
 func (tm TestMsg) Payload() []byte {
-	return []byte{}
+	return tm.PayloadBytes
 }
 func (tm TestMsg) Ack() {
 	return
@@ -43,21 +46,32 @@ func TestMeasurementReader(t *testing.T) {
 	// setup
 	reg := prometheus.NewRegistry()
 	metrics := NewMetrics(reg)
-
-	readMeasurement := measurementReader(metrics)
-	c := mqtt.NewClient(mqtt.NewClientOptions())
-	msg := TestMsg{}
-	readMeasurement(c, msg)
-
 	ts := httptest.NewServer(promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 	defer ts.Close()
 
-	resp, err := http.Get(ts.URL)
-	assert.NoError(err)
+	readMeasurement := measurementReader(metrics)
+	c := mqtt.NewClient(mqtt.NewClientOptions())
 
-	body, err := io.ReadAll(resp.Body)
-	assert.NoError(err)
+	testCases := []struct {
+		msg      TestMsg
+		expected string
+	}{
+		{TestMsg{TopicString: "", PayloadBytes: []byte{}}, "outdoor_temperature_celsius 32"},
+	}
 
-	//t.Logf("body: %+v\n", string(body))
-	assert.Contains(string(body), "outdoor_temperature_celsius 32")
+	for _, tc := range testCases {
+		t.Run(tc.expected, func(t *testing.T) {
+			readMeasurement(c, tc.msg)
+
+			resp, err := http.Get(ts.URL)
+			assert.NoError(err)
+
+			body, err := io.ReadAll(resp.Body)
+			assert.NoError(err)
+
+			//t.Logf("body: %+v\n", string(body))
+			assert.Contains(string(body), tc.expected)
+		})
+	}
+
 }
